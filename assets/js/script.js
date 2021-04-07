89f61517f5be0200ad3b8650e9ad04b76cd1856e
NicolaDiSante <nickymacha90@gmail.com>
2021-04-07 16:42:12 +0200

    Final Work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
+
+
 var numKm = parseInt(prompt("Quanti km percorrerai?"));
 var eta = parseInt(prompt("Quanti anni hai?"));
 var prezzo = numKm * 0.21;
 
+if(eta == 0){
+  alert("Error!")
+}
+
 if(eta<18){
-  console.log(prezzo * 80/100)
+  prezzo=(prezzo * 80/100)
 }else if(eta>65){
-  console.log(prezzo * 60/100)
-}else{
-  console.log(prezzo)
+  prezzo=(prezzo * 60/100)
 }
 
 
-document.getElementById('msgInfo').innerHTML = "Il prezzo totale del viaggio sarà " + prezzo;
+document.getElementById('msgInfo').innerHTML = "Il prezzo totale del viaggio sarà " + Math.round(prezzo);
+
